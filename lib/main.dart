c46ea234feb3651aa205a70ab9b4afa01e9fd02b
biags72 <bia.gutierrez7@gmail.com>
2023-04-12 01:39:28 -0700

    Reorganizing github repo

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertest/pages/Home_page.dart';
+import 'package:fluttertest/pages/login_page.dart';
+
+void main() => runApp( MyApp()
+);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: MyHomePage(),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertest/components/button.dart';
+import 'package:fluttertest/components/text_field.dart';
+import 'package:fluttertest/components/square_tile.dart';
+
+
+  class login_page extends StatelessWidget {
+    login_page({Key? key}) : super(key: key);
+
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView( child:SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 50),
+
+              // logo
+              Icon(
+                Icons.lock,
+                size: 100,
+              ), //icon
+
+              SizedBox(height: 50),
+              // welcome back, you've been missed!
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ), //text
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                      hintText: 'Enter valid mail id as abc@gmail.com'
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter your secure password'
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              // sign in button
+              button(
+                onTap: signUserIn,
+              ), //button
+
+              const SizedBox(height: 50),
+
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ), //divider
+                    ),//expanded
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or Continue With',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),//text
+                    ), //padding
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),//divider
+                    ),//expanded
+                  ],
+                ),//row
+              ),//padding
+
+              const SizedBox(height: 50),
+
+              // google and facebook sign in buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  // google button
+                  square_tile(imagePath: 'lib/images/google.png'),
+
+                  SizedBox(width: 10),
+
+                  // facebook button
+                  square_tile(imagePath: 'lib/images/facebook.png'),
+
+                  SizedBox(width: 10),
+                ],
+              ),
+
+              const SizedBox(height: 50),
+
+              // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      ),
+    );
+  }
+}
