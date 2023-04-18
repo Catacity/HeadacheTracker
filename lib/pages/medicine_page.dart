34584b17a593962322c81d180519d5281893ba97
Catacity <wendyhui2371780044@gmail.com>
2023-04-18 22:37:50 +0800

    Medicine page basic UI done

--- lib/pages/medicine_page.dart
+++ lib/pages/medicine_page.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 DateTime now = DateTime.now();
 DateTime todayDate = DateTime(now.year,now.month,now.day);
 TimeOfDay TODNow = TimeOfDay(hour: now.hour, minute: now.minute);
+List<Color> colors = [Colors.lightBlueAccent, Colors.blueAccent];
 
 class MedicineFormMenu extends StatefulWidget {
   @override
@@ -16,7 +19,10 @@ class _MedicineFormState extends State<MedicineFormMenu> {
   DateTime _MedicineDate = todayDate;
   TimeOfDay _TODMedicine = TODNow;
 
-  @override
+  // Meaning the partial / full mutually exclusive buttons are clickable
+  bool _ispartial = false;
+  bool _isfull = false;
+
   void initState() {
     // Ctor
     super.initState();
@@ -86,6 +92,7 @@ class _MedicineFormState extends State<MedicineFormMenu> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: buildAppBar(),
       body: Container(
         decoration: const BoxDecoration(
@@ -162,24 +169,24 @@ class _MedicineFormState extends State<MedicineFormMenu> {
                               ),
                             ]
                         ),
-                        // Row(
-                        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //   children: <Widget>[
-                        //     Container(
-                        //       padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
-                        //       child:
-                        //       Padding(
-                        //         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
-                        //         child: TextFormField(
-                        //           decoration: const InputDecoration(
-                        //             border: UnderlineInputBorder(),
-                        //             labelText: 'Please type in the medicine you have taken..'
-                        //           ),
-                        //         ),
-                        //       ),
-                        //       ),
-                        //     ]
-                        // ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              // Wrapping the text box with a fixed height and width is necessary
+                              width: 340,
+                              height: 40,
+                              padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                              child: TextField(
+                                controller: _medicineTextController,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: 'Please list out the medicine taken..'
+                                ),
+                              ),
+                            ),
+                          ]
+                        ),
                       ]
                   ),
                 )
@@ -188,6 +195,144 @@ class _MedicineFormState extends State<MedicineFormMenu> {
             ),
 
             // Row 3
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 350,
+                    child: SizedBox(
+                      height: 100,
+                      child: Container(
+                        decoration:BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(10)
+                            ),
+                            color: Colors.white
+                        ),
+                        child: Column(
+                            children: <Widget>[
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Container(
+                                      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                                      child: Icon(
+                                        Icons.percent_outlined,
+                                        color: Colors.orange,
+                                        size: 25.0,
+                                      ),
+                                    ),
+                                    Container(
+                                      child: Text("Pain relief?",
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'ComicNeue',
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      child: IconButton(
+                                        onPressed: () {
+                                          showDialog(
+                                              context: context,
+                                              builder: (context) => AlertDialog(
+                                                title: Text("Headache Levels"),
+                                                content: Text(
+                                                  "Partial relief:.. \n\nFull relief:.. \n\n",
+                                                ),
+                                                actions: [
+                                                  TextButton(
+                                                    child: Text("OK"),
+                                                    onPressed: () => Navigator.pop(context),
+                                                  ),
+                                                ],
+                                              )
+                                          );
+                                        },
+                                        icon: Icon(
+                                          Icons.question_mark_outlined,
+                                          size: 25.0,
+                                        ),
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ]
+                              ),
+                              Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Container(
+                                      height: 50,
+                                      width: 350,
+                                      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(10)
+                                          ),
+                                          color: Colors.transparent
+                                      ),
+                                      child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Container(
+                                              // padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                                              child: ElevatedButton(
+                                                  onPressed: () {
+                                                    setState(() => _ispartial = true);
+                                                    setState(() => _isfull = false);
+                                                  },
+                                                  child: Text("Partial",
+                                                    style: TextStyle(
+                                                        fontSize: 20.0,
+                                                        fontWeight: FontWeight.bold,
+                                                        fontFamily: 'ComicNeue',
+                                                        color: Colors.black
+                                                    ),
+                                                  ),
+                                                  style: ButtonStyle(
+                                                    backgroundColor: MaterialStateProperty.all<Color>(colors[(_ispartial)? 1 : 0]),
+                                                  )
+                                              ),
+                                            ),
+
+                                            Container(
+                                              // padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                                              child: ElevatedButton(
+                                                  onPressed: () {
+                                                    setState(() => _ispartial = false);
+                                                    setState(() => _isfull = true);
+                                                  },
+                                                  child: Text("  Full  ",
+                                                    style: TextStyle(
+                                                        fontSize: 20.0,
+                                                        fontWeight: FontWeight.bold,
+                                                        fontFamily: 'ComicNeue',
+                                                        color: Colors.black
+                                                    ),
+                                                  ),
+                                                  style: ButtonStyle(
+                                                    backgroundColor: MaterialStateProperty.all<Color>(colors[(_isfull)? 1 : 0]),
+                                                  )
+                                              ),
+                                            ),
+                                          ]
+                                      ),
+
+
+                                    ),
+                                  ]
+                              ),
+                            ]
+                        ),
+                      )
+                    ),
+                  ),
+                ]
+
+            ),
+
+            // Row 4
             Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
@@ -280,7 +425,7 @@ class _MedicineFormState extends State<MedicineFormMenu> {
                 ]
             ),
 
-            // Row 4
+            // Row 5
             Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -369,6 +514,70 @@ class _MedicineFormState extends State<MedicineFormMenu> {
               ),
             ]
         ),
+
+            // Row 6
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    width: 350,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(10)
+                        ),
+                        color: Colors.transparent
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            // padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  // Confirm... Need to pass value back to the headache form
+                                },
+                                child: Text("Confirm",
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'ComicNeue',
+                                      color: Colors.black
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreenAccent),
+                                )
+                            ),
+                          ),
+
+                          Container(
+                            // padding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  // Cancel
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("Cancel",
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'ComicNeue',
+                                      color: Colors.black
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                                )
+                            ),
+                          ),
+                        ]
+                    ),
+
+
+                  ),
+                ]
+            ),
         ]
       )
      )
