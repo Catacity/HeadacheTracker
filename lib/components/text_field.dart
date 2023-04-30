c9c6f267090d21cea48f01ed77a3d8169446a42f
biags72 <69768507+biags72@users.noreply.github.com>
2023-04-30 06:27:25 -0700

    Add files via upload

--- lib/components/text_field.dart
+++ lib/components/text_field.dart
@@ -1,64 +1,45 @@
 import 'package:flutter/material.dart';
-import 'helptofill.dart';
 
 class text_field extends StatelessWidget {
-  //const text_field({Key? key}) : super(key: key);
-  TextEditingController controller;
-  String hintName;
-  IconData icon;
-  bool isObscureText;
-  TextInputType inputType;
-  bool enable;
-  var validator;
-  var decoration;
+  final controller;
+  final String hintText;
+  final bool obscureText;
 
-  text_field(
-      { required this.controller,
-        required this.hintName,
-        required this.icon,
-        this.isObscureText = false,
-        this.inputType = TextInputType.text,
-        this.enable = true,
-        //required keyboardType,
-        this.decoration,
-        this.validator
-      });
-
-  get isEnable => null;
+  const text_field({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    required InputDecoration decoration,
+  });// {
+  //TODO: implement TextField
+//    throw UnimplementedError();
+  // }// {
+  //TODO: implement TextField
+  //throw UnimplementedError();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20.0),
-      child: text_field(
-        controller: controller,
-        isObscureText: isObscureText,
-        enable: isEnable,
-        inputType: TextInputType.text,
-        validator: (value) {
-           if (value == null || value.isEmpty) {
-             return 'Please enter $hintName';
-           }
-           if (hintName == "Email" && !validateEmail(value)) {
-             return 'Please Enter Valid Email';
-           }
-             return null;
-        },
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            borderSide: BorderSide(color: Colors.transparent),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(30.0)),
-            borderSide: BorderSide(color: Colors.blue),
-          ),
-        prefixIcon: Icon(icon),
-        hintText: hintName,
-        labelText: hintName,
-        fillColor: Colors.grey[200],
-        filled: true
-        ), hintName: '', icon: icon,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          controller: controller,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400),
+              ),
+              fillColor: Colors.grey.shade200,
+              filled: true,
+              hintText: hintText,
+              hintStyle: TextStyle(color: Colors.grey[500]))
+        ),
       ),
     );
   }
